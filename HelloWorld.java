1fbae941ec043fe77ac07b8b6cf602a5804314fa
root <root@uddhav-HP-Pavilion-g6-Notebook-PC>
2017-09-06 15:42:31 -0400

    corrected

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.printllln("Hello, World");
+        System.out.println("Hello, World");
     }
 
 }
