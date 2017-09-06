a837941ad57511f613307a06ab7085e61d866a67
Uddhav Gautam <upgautam@ualr.edu>
2017-09-06 13:38:58 -0400

    wrong updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.printllln("Hello, World");
+        System.out.prinsdfsdfsdfsdfdsfsdfdsftllln("Hello, World");
     }
 
 }
